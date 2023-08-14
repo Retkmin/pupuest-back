85e7175707ee4547ee47bf98ca7274b57402ad8f
albertpm95 <albertpm95@outlook.com>
2023-08-14 21:05:03 +0200

    Cors

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Pupuest", debut=True)
 
 from routers import login
 
 app.include_router(login.router)
+app.include_router(login.router)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+async def root():
+    raise HTTPException(status_code=200, detail="Hay conexion con el servidor.")
