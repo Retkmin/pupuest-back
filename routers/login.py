a2d5f38d7fffb149a01cb8a4361e2832ba9da387
albertpm95 <albertpm95@outlook.com>
2023-08-15 13:44:17 +0200

    Metadata for openapi docs

--- routers/login.py
+++ routers/login.py
@@ -6,6 +6,15 @@ router = APIRouter(prefix="/login", tags=["Login"])
 
 @router.post("", response_model=Token)
 async def login_for_access_token():
+    """
+    Login with user and password and return a token
+
+    - **username**: The username
+    - **password**: The password
+    - **access_token**: The Token
+    \f
+    :param usuario_nuevo: User input.
+    """
     raise HTTPException(
             status_code=status.HTTP_501_NOT_IMPLEMENTED,
             detail="Login securizado todavia no implementado.",
