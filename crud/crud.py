fec87d16914c6422e9a6422eb9877fe903c233b0
albertpm95 <albertpm95@outlook.com>
2023-08-15 14:08:04 +0200

    General crud operations

---
+++ crud/crud.py
@@ -0,0 +1,26 @@
+from fastapi import HTTPException, status
+
+from db.database import SessionLocal
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def verify_password(plain_password, usuario_db_hashed_password):
+    if get_password_hash(plain_password) != usuario_db_hashed_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuario o contraseña incorrectos.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return True
+
+
+def get_password_hash(plain_password: str):
+    return plain_password
