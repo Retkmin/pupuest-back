242e65a780165b6b4d98bb8173cb577a03a73121
albertpm95 <albertpm95@outlook.com>
2023-08-15 18:33:00 +0200

    User model

--- models/user.py
+++ models/user.py
@@ -6,7 +6,8 @@ from sqlmodel import Field, SQLModel
 
 class User(SQLModel, table=True):
     
-    __tablename__ = "users"
+    __tablename__ = "Users"
+    
     id_user: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(nullable=False) 
     id_password: int = Field(nullable=False)
@@ -16,5 +17,5 @@ class User(SQLModel, table=True):
     reset_token: str = Field(nullable=False)
     verification_token: str = Field(nullable=False)
     email: str = Field(nullable=False)
-    created_at: datetime = Field(nullable=False)
-    updated_at: datetime = Field(nullable=False)
+    created_at: datetime = Field(nullable=False, default=datetime.datetime.now())
+    updated_at: datetime = Field(nullable=False, default=datetime.datetime.now())
