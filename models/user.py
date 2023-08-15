2ce927043cd9ae6c142933a0333fb4d9ab67bf6d
albertpm95 <albertpm95@outlook.com>
2023-08-15 16:12:39 +0200

    Updated user model with : table name Created and updated datetime fields Default values Nullability

--- models/user.py
+++ models/user.py
@@ -1,15 +1,20 @@
+import datetime
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
+    
+    __tablename__ = "users"
     id_user: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field()
-    id_password: int = Field()
-    is_active: bool = Field()
-    is_staff: bool = Field(default=False)
-    is_active: bool = Field(default=False)
-    reset_token: str = Field()
-    verification_token: str = Field()
-    email: str = Field()
+    username: str = Field(nullable=False) 
+    id_password: int = Field(nullable=False)
+    is_active: bool = Field(nullable=False, default=False)
+    is_staff: bool = Field(default=False, nullable=False)
+    is_active: bool = Field(default=False, nullable=False)
+    reset_token: str = Field(nullable=False)
+    verification_token: str = Field(nullable=False)
+    email: str = Field(nullable=False)
+    created_at: datetime = Field(nullable=False)
+    updated_at: datetime = Field(nullable=False)
