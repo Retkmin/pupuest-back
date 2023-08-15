7279e653976dfa8b7dda29264fd64350adc4afe4
albertpm95 <albertpm95@outlook.com>
2023-08-15 13:44:07 +0200

    Metadata Openapi docs

--- routers/operations.py
+++ routers/operations.py
@@ -5,7 +5,22 @@ from models.operation import Operation
 router = APIRouter(prefix="/operations", tags=["Operation"])
 
 @router.post("", response_model=Operation)
-async def login_for_access_token():
+async def create_operation_based_on_strategy(operation: Operation):
+    """
+    Create an operation based on a strategy
+
+    - **operative_id**: Optional[int] 
+    - **strategy_id**: int
+    - **stop_lose_price**: float
+    - **max_value**: float
+    - **entry_price**: float
+    - **opening_datetime**: datetime
+    - **closing_datetime**: datetime
+    - **asset**: str
+    - **operation_type**: str
+    \f
+    :param operation: User input.
+    """
     raise HTTPException(
             status_code=status.HTTP_501_NOT_IMPLEMENTED,
             detail="Saving an operation is not implemented yet.",
