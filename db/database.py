334b41a824ee3a9fc9433dd0fe0466178691f8d7
Albert <albertpm95@outlook.com>
2023-08-21 17:26:11 +0200

    DB Connection established

--- db/database.py
+++ db/database.py
@@ -1,29 +1,30 @@
-from sqlmodel import MetaData, Session, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import MetaData, SQLModel, create_engine
 
-user = "postgres"
+from models import (currency, invoice, language, operation,  # noqa: F401
+                    payment_method, theme, token, user, user_info)
+
+dbuser = "postgres"
 password = "axzCWh766"
 host = "tradesage-dev.c0xcfmrj3pzo.us-east-2.rds.amazonaws.com"
 port = "5432"
-database = "tradesage-dev"
+database = "postgres"
 
-DATABSE_URI: str = (
-    "postgresql://"
-    + user
-    + ":"
-    + password
-    + "@"
-    + host
-    + ":"
-    + port
-    + "/"
-    + database
-)
+#DATABASE_URI: str = ("postgresql://" + user + ":" + password + "@" + host + "/" + database)
 
+#DATABASE_URI = "postgresql://postgres:axzCWh766@tradesage-dev.c0xcfmrj3pzo.us-east-2.rds.amazonaws.com/postgres"
+
+DATABASE_URI = f"postgresql://{dbuser}:{password}@{host}:{port}/{database}"
 metadata = MetaData(schema="public")
 
-engine = create_engine(DATABSE_URI)
+engine = create_engine(
+    DATABASE_URI,
+    connect_args={"options": "-csearch_path=public"}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
+SQLModel.metadata = Base.metadata
 
-def get_session():
-    with Session(engine) as session:
-        yield session
+#SQLModel.metadata.create_all(engine)
