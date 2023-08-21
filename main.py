7692cdff766ca100a01d95f722dee25983c71fdf
Albert <albertpm95@outlook.com>
2023-08-21 17:30:32 +0200

    Fix docs bad meta in contact

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-from crud import crud
 from routers import login, operations, users
 
-app = FastAPI(title="Pupuest", debut=True,
+app = FastAPI(
+    title="Pupuest",
+    debut=True,
     description="Descripcion del proyecto",
     summary="Diego bobo",
     version="0.0.1",
     contact={
-        "name": "Dario bobo",
-        "url": "www.twitter.com/rektmin",
-        "email": "diegomp96@gmail.com",
-    },
+        "name": "Rektmin",
+        "email": "diegomp96@gmail.com"
+    }
 )
-
-
 
 app.include_router(login.router)
 app.include_router(operations.router)
@@ -29,10 +27,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 @app.get("/")
 async def root():
-    try:
-        crud.get_db()
-    finally:
-        raise HTTPException(status_code=500, detail="Coudln't connect to the database.")
-    raise HTTPException(status_code=200, detail="The backend is online.")    
+    raise HTTPException(status_code=200, detail="The backend is online.")
