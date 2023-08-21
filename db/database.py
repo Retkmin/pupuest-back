8cd942c131412ebcf6de7e5f982f05dc7736eefd
Albert <albertpm95@outlook.com>
2023-08-21 15:50:43 +0200

    Changed variable name

--- db/database.py
+++ db/database.py
@@ -1,22 +1,30 @@
 from sqlmodel import MetaData, Session, create_engine
 
-#"postgresql://user:password@postgresserver/db"
-user="postgres"
-password="axzCWh766"
-url="tradesage-dev.c0xcfmrj3pzo.us-east-2.rds.amazonaws.com/"
-port="5432"
-database="tradesage-dev"
+# "postgresql://user:password@postgresserver/db"
+user = "postgres"
+password = "axzCWh766"
+postgresserver = "tradesage-dev.c0xcfmrj3pzo.us-east-2.rds.amazonaws.com/"
+port = "5432"
+database = "tradesage-dev"
 
-DATABSE_URI: str = "postgresql+psycopg2://"+user+":"+password+"@"+url+":"+port+"/"+database
-
-#conn = psycopg2.connect(DATABSE_URI)
+DATABSE_URI: str = (
+    "postgresql://"
+    + user
+    + ":"
+    + password
+    + "@"
+    + postgresserver
+    + ":"
+    + port
+    + "/"
+    + database
+)
 
 metadata = MetaData(schema="public")
 
-
 engine = create_engine(DATABSE_URI)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
-    
