7a0716bd8069ededc407cc234f8a624aab9aaf3f
Albert <albertpm95@outlook.com>
2023-08-26 18:23:56 +0200

    Changed user router and crud

--- crud/user.py
+++ crud/user.py
@@ -7,13 +7,9 @@ def get_users_list(session: Session) -> list[User]:
     return session.query(User).all()
 
 def create_user(session: Session, new_user: User):
-    print("\n Antes de añadir \n")
-    print(new_user)
     session.add(new_user)
     session.commit()
-    #session.refresh(new_user)
-    print("\n Despues de añadirlo\n")
-    print(new_user)
-    #session.close()
+    session.refresh(new_user)
+    session.close()
     return new_user
         
--- routers/users.py
+++ routers/users.py
@@ -7,19 +7,8 @@ from models.user import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/create", response_model=User)
+@router.post("/create")
 async def create_user(user: User, session: Session() = Depends(crud.get_session)):
-    """
-    Create an User with all the information:
-
-    - **nombre**: El nombre
-    - **apellidos**: Los apellidos del usuario
-    - **username**: El username con el que iniciara sesion.
-    - **rol**: El rol del usuario
-    - **estado**: El estado del usuario.
-    \f
-    :param usuario_nuevo: User input.
-    """
     return crud_user.create_user(session=session, new_user=user)
     
 @router.get("/list", response_model=list[User])
