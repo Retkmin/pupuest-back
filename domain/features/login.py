4ebc0fdacf732e832c097c3a09d336af490c0b0a
Albert <albertpm95@outlook.com>
2023-08-27 20:45:46 +0200

    Base login

--- domain/features/login.py
+++ domain/features/login.py
@@ -1,32 +1,35 @@
+
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
 from sqlmodel import Session, SQLModel
 
 from domain.core.crud import get_session
 
 router = APIRouter(prefix="/login", tags=["Login"])
 
-class UserPassword(SQLModel):
-    username: str
-    password: str
-    
-@router.post("/login")
-async def login(form_data: UserPassword, session: Session = Depends(get_session)):
-    check_password(form_data)
-    raise HTTPException(status_code=500, detail="Not implemented yet.")
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-def check_password(password: str):
-    hashed_value = calculate_hash(password)
-    token = ""
-    
-    if validate_password(password, hashed_value):
-        return token
-    
-    raise HTTPException(status_code=403, detail="Username or password not valid")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def calculate_hash(password: str):
-    return password + "123"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def validate_password(password, hashed_value):
-    if password == (hashed_value+"123"):
-        return True
-    return False
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(SQLModel):
+    username: str | None = None
+    
+
+@router.post("", description="API Endpoint to login as a user.",
+    summary="Endpoint to login as a user."
+)
+async def login_for_access_token(
+    formData: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_session)
+) -> Token:
+    return HTTPException(status_code=501)
