ce4e43d40d3198403b1465e6df69b931e90d3900
Albert <albertpm95@outlook.com>
2023-09-02 14:23:46 +0200

    Added reset and set password

--- data/core_aws_postgres/aws_db_models/password/password_crud.py
+++ data/core_aws_postgres/aws_db_models/password/password_crud.py
@@ -5,16 +5,29 @@ import string
 from sqlmodel import Session
 
 from data.core_aws_postgres.aws_db_models.password.password import Password
+from data.core_aws_postgres.aws_db_models.user.user import User
 
 
 def get_password_by_id(session: Session, id_password: int):
     return session.get(Password, id_password)
 
 def generate_random_password(length=12):
-    
     characters = string.ascii_letters + string.digits + string.punctuation
-    print("\n Generating random password characters:", characters)
-    
     password = ''.join(random.choice(characters) for _ in range(length))
-    print("\n Temporary password created:", password, "\n")
     return password
+
+def reset_password(session: Session, user: User):
+    password_exists = get_password_by_id(session, user.id_password)
+    password_exists.password_hash = None
+    session.add(password_exists)
+    session.commit()
+    session.refresh(password_exists)
+    return True
+
+def set_new_password(session: Session, user: User, hashed_password: str):
+    password_exists = get_password_by_id(session, user.id_password)
+    password_exists.password_hash = hashed_password
+    session.add(password_exists)
+    session.commit()
+    session.refresh(password_exists)
+    return True
